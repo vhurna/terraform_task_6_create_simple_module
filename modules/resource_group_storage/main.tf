7a1f7541eb3220c10a2063d45aa738989c0f14db
mira <mkyshchak@gmail.com>
2025-05-15 21:30:56 +0300

    'module'

---
+++ modules/resource_group_storage/main.tf
@@ -0,0 +1,12 @@
+resource "azurerm_resource_group" "this" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "this" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.this.name
+  location                 = azurerm_resource_group.this.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
---
+++ modules/resource_group_storage/outputs.tf
@@ -0,0 +1,7 @@
+output "resource_group_id" {
+  value = azurerm_resource_group.this.id
+}
+
+output "storage_account_id" {
+  value = azurerm_storage_account.this.id
+}
